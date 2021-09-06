95dc03a7761db2d7d3bed805d85b7c8dc8d846c3
Rijen Savsani <55935083+rijen17@users.noreply.github.com>
2021-09-06 12:54:51 +0530

    Add files via upload

---
+++ grails-app/services/grails/rest/Industryproject_domainService.groovy
@@ -0,0 +1,41 @@
+package demo.api
+
+import grails.gorm.transactions.Transactional
+import groovy.sql.Sql
+@Transactional
+class Industryproject_domainService {
+
+    def list(def params, def request) {
+        return Industryproject_domain.findAll()
+    }
+
+    def single(def params, def request) {
+        return Industryproject_domain.findById(params?.id)
+    }
+
+    def save(def params, def request) {
+        def ipJson = request.JSON
+        def ipInstance = new Industryproject_domain(ipJson)
+
+        ipInstance = ipInstance.save()
+
+        return ipInstance
+    }
+
+    def update(def params, def request) {
+        def ipJson = request.JSON
+        def ipInstance = Industryproject_domain.get(params?.id)
+        ipInstance.properties = ipJson
+
+        ipInstance = ipInstance.merge()
+
+        return ipInstance
+    }
+
+    def delete(def params, def request) {
+        def ipInstance = Industryproject_domain.get(params?.id)
+        ipInstance = ipInstance.delete()
+
+        return ipInstance
+    }
+}
